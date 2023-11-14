5464ded51dfe1f5cbdd0532f5ca71d2722b4cc8d
adraarda23 <ardaaydinkilinc@gmail.com>
2023-11-14 03:36:02 +0300

    file hierarchy changes

---
+++ Notes/BigO.js
@@ -0,0 +1,18 @@
+/*
+n value is equals to iteration duration
+Constants doesnt matter
+O(2n) ==> O(n)  or O(n/2)==> O(n)
+O(n^2+n)==> O(n^2)
+*/
+
+/* SPACE COMPLEXITY
+n value is equal to variables created for hold values
+*/
+
+/*
+pop and push O(1)
+shift and unshift O(n) because of index it have to iterate
+*/
+
+
+
